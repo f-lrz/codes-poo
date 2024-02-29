45eb87a240023c2da7c4770c74dfb4a26e33f5a9
filipe <filipelorenzoarraes@gmail.com>
2024-02-29 12:16:57 -0300

    .

---
+++ pratice.java
@@ -0,0 +1,29 @@
+package vetor;
+
+import java.util.Scanner;
+
+public class Vetorclass {
+
+	public static void main(String[] args) {
+		int vetor[] = new int[5];
+		Scanner sc = new Scanner(System.in);
+		
+		int soma = 0;
+		
+		for(int i = 0; i < vetor.length; i++) {
+			System.out.println("Informe um numero inteiro: ");
+			vetor[i] = sc.nextInt();
+			soma += vetor[i];
+		}
+		System.out.println(soma);
+		
+		soma = 0;
+		
+		for(int v : vetor) {
+			soma += v;
+		}
+		System.out.println(soma);
+		
+	}
+
+}
